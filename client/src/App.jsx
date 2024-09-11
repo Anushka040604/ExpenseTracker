99a24d328b55b1fd30caac506cf25d65d2754883
Anushka Bachhav <anushkabachhav62@gmail.com>
2024-09-11 18:52:44 +0530

    expense tracker

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    
+      <div className='App bg-#1e1b4b'>
+        <h1 className="text-3xl font-bold underline ">Expense Tracker</h1>
+      </div>
+
+  )
+}
+
+export default App
